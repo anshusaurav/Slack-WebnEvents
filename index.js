5e63cf60a25c23473df0f003f332fe8cd2983f2a
anshusaurav <anshu.saurav@gmail.com>
2020-09-23 19:01:56 +0530

    Slack channels n Members

---
+++ index.js
@@ -0,0 +1,91 @@
+
+var channel = "bot-test";
+// var SlackBot = require("slackbots");
+// var bot = new SlackBot({
+//     token: "xoxb-401428056419-1405682548464-f7cCISzdSSYJHqe6IDG5eq9Q",
+//     name: "trialbot"
+//     // token: "xoxb-401428056419-1366979354263-dkCXZvk4zCI5V646rgBsSEsp",
+//     // name: "jokebot"
+// });
+// const channels = [];
+// bot.on("start", function () {
+//     bot.postMessageToChannel(channel, "Hello world!");
+//     console.log("Hello world!");
+//     bot.getChannels().then(function (data) {
+//         const { channels } = data;
+//         channels.forEach(channel => console.log(channel.id + ' ' + channel.name));
+//     })
+
+
+//     bot.getUsers().then(function (data) {
+//         const { members } = data;
+//         members.forEach(user => console.log(user.id + ' ' + user.name + ' ' + user.real_name + ' ' + user.tz));
+//     })
+// });
+
+
+
+// const token = "xoxb-401428056419-1366979354263-dkCXZvk4zCI5V646rgBsSEsp"
+// const Slack = require('slack')
+// const bot = new Slack({ token })
+
+// // logs {args:{hyper:'card'}}
+// slack.channels.history({ token, channel })
+// bot.api.test({ hyper: 'card' }).then(console.log)
+
+// CBT9CGUBE general
+// CBUHLH8TZ random
+// CCKNFJM5E html-css
+// CCLAMBM0B building-portfolio
+// CCLJWU5HT javascript
+// CDDDBAV0B bde_log
+// CE0J37WUC front-end
+// CEAV1UWDV coding-challenges
+// CEK6ABXME datagov
+// CF06LEM6D the-creatives
+// CF0GV9VU2 build_hours
+// CH5AL8EHW backend
+// CJJA75Z1C batch
+// CU3UW6H7Z batch-12
+// CUG298HE1 public-notes
+// C0183SD6LF3 job-readiness-challenge
+// C01B8HWFN49 bot-test
+
+const { WebClient } = require('@slack/web-api');
+
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const token = "xoxb-401428056419-1366979354263-dkCXZvk4zCI5V646rgBsSEsp";
+
+const web = new WebClient(token);
+// web.conversations.list().then(function (res) {
+//     const { channels } = res;
+//     channels.forEach(channel => console.log(channel.id + ' ' + channel.name));
+//     let requests = channels.map(channel => web.conversations.members(channel.id)
+//     );
+// })
+
+
+const getChannels = () => {
+    web.conversations.list().then(function (res) {
+        const { channels } = res;
+        channels.forEach(channel => console.log(channel.id, channel.name, channel.is_archived, channel.num_members));
+
+    })
+}
+const getChannelWithMembers = async () => {
+    web.conversations.list().then(function (res) {
+        const { channels } = res;
+        console.log('All Channels')
+        channels.forEach(channel => console.log(channel.id + ' ' + channel.name));
+        let requests = channels.filter(channel => !channel.is_archived).map(channel => {
+            console.log(channel.id)
+            web.conversations.members({ channel: channel.id })
+        });
+
+    })
+    // // channels.forEach(channel => console.log(channel.id + ' ' + channel.name));
+    // const resTwo = await web.conversations.members({ channel: 'C0183SD6LF3' });
+    // console.log(resTwo);
+}
+getChannels();
+getChannelWithMembers();
